979545e43d63bcfd15a30fc184369d862c233b9e
Aashima Grover <aashimagrover.work@gmail.com>
2026-08-27 11:33:20 +0530

    Fix comments in script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-  */ '-' was missing 
+  /* '-' was missing */
   $('#login-form').submit(function (e) {
 
-  */ prevents the form from refreshing so that we are able to see the validation messages 
+  /* prevents the form from refreshing so that we are able to see the validation messages */
   e.preventDefault();
 
     var email = $('#email').val();
@@ -10,11 +10,11 @@ $(document).ready(function () {
     var isValid = true;
 
     if (email == '') {
-      */ 'error' spelling is wrong so the message would never display
+      /* 'error' spelling is wrong so the message would never display */
       $('#email-error').show().text('Email is required');
       isValid = false;
     }
-    */ the error message says atleast six characters so length should be less than 6 not 5
+    /* the error message says atleast six characters so length should be less than 6 not 5 */
     if (password.length < 6) {
       $('#password-error').show().text('Password must be at least 6 characters');
       isValid = false;
